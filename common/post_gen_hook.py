1f532b1bb7cc741c211d0214d9a69856d04828c3
Cory Perkins <link2cory@gmail.com>
2020-07-04 13:39:46 -0700

    fixed helper functions so they no longer rely on the no longer present, Jinja template object

--- common/post_gen_hook.py
+++ common/post_gen_hook.py
@@ -6,7 +6,15 @@ from cookiecutter.main import cookiecutter
 from github import Github
 from git import Repo
 
-def setup_git(use_github, continuous_integration, use_gitflow): 
+def setup_git(context): 
+    project_slug = context['project_slug']
+    use_github = context['use_github']
+    continuous_integration = context['continuous_integration']
+    use_gitflow = context['use_gitflow']
+    github_token = context['github_token']
+    github_user = context['github_user']
+    ci_template = context['ci_template']
+    ci_test_name = context['ci_test_name']
     # activate .gitignore. It must be stored this way to facilitate versioning of
     # the cookiecutter.
     os.rename('gitignore', '.gitignore')
@@ -21,13 +29,13 @@ def setup_git(use_github, continuous_integration, use_gitflow):
 
     if use_github == 'yes':
         print('setting up github repository...')
-        remote = setup_remote(local)
-        add_remote(local)
+        remote = setup_remote(github_token, project_slug)
+        add_remote(local, github_user, project_slug)
         print('done setting up github repository')
         
         if continuous_integration != 'no':
             print('setting up continuous integration...')
-            setup_continuous_integration(local, remote)
+            setup_continuous_integration(local, remote, ci_template, ci_test_name)
             print('done setting up continuous integration...')
 
         if use_gitflow == 'yes':
@@ -37,9 +45,9 @@ def setup_git(use_github, continuous_integration, use_gitflow):
 
 
 
-def setup_remote(local):
+def setup_remote(github_token, project_slug):
     # create the github repo
-    return Github('{{ cookiecutter.github_token }}').get_user().create_repo('{{ cookiecutter.project_slug }}')
+    return Github(github_token).get_user().create_repo(project_slug)
 
 def setup_gitflow(remote):
     # create the develop branch
@@ -52,12 +60,12 @@ def setup_gitflow(remote):
     remote.edit(default_branch='develop')
 
 
-def setup_continuous_integration(local, remote):
-    ci_context = {"test_name": "{{ cookiecutter.ci_test_name }}"}
+def setup_continuous_integration(local, remote, ci_template, ci_test_name):
+    ci_context = {"test_name": ci_test_name}
 
     # add the CI files from the template
     success = cookiecutter(
-        context['ci_template'],
+        ci_template,
         extra_context=ci_context,
         no_input=True
     )
@@ -69,14 +77,14 @@ def setup_continuous_integration(local, remote):
     # the default branch should be protected against pull requests that fail CI tests
     remote.get_branch(remote.default_branch).edit_protection(
             strict=True,
-            contexts=['{{ cookiecutter.ci_test_name }}']
+            contexts=[ci_test_name]
         )
 
-def add_remote(local):
+def add_remote(local, github_user, project_slug):
     # point local repo to the remote
     remote_repo = local.create_remote(
         'origin',
-        'git@github.com:{{ cookiecutter.github_user }}/{{ cookiecutter.project_slug }}.git'
+        f'git@github.com:{github_user}/{project_slug}.git'
     )
 
     #  push to the remote
@@ -125,7 +133,7 @@ def main(context):
 
       if context['use_git'] == 'yes':
           print('setting up git...')
-          setup_git(context['use_github'], context['continuous_integration'], context['use_gitflow'])
+          setup_git(context)
           print('finished setting up git...')
     else:
         remove_project_config_files()

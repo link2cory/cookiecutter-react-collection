7eaa785206ee85ff357d0208f4112a401dea7773
Cory Perkins <link2cory@gmail.com>
2020-07-19 14:40:47 -0700

    add tailwind styled component support to cookiecutter-react-functional-component

--- cookiecutter-react-functional-component/{{cookiecutter.component_name}}/{{cookiecutter.component_name}}.styles.tsx
+++ cookiecutter-react-functional-component/{{cookiecutter.component_name}}/{{cookiecutter.component_name}}.styles.tsx
@@ -1,5 +1,11 @@
+{% if cookiecutter.tailwindcss == 'no' %}
 import styled from 'styled-components'
 
 const Styled{{ cookiecutter.component_name }} = styled.div``
+{% else %}
+import tw from 'twin.macro'
+
+const Styled{{ cookiecutter.component_name }} = tw.div``
+{% endif %}
 
 export default Styled{{ cookiecutter.component_name }}

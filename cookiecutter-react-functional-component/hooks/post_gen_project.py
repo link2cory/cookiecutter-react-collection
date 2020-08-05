ad37a1704eeb6ceaa8634864f7e1de1ac4352734
Cory Perkins <link2cory@gmail.com>
2020-08-04 19:25:10 -0700

    cookiecutter-react-functional-component: rename component files to describe which part of the component is being defined

--- cookiecutter-react-functional-component/hooks/post_gen_project.py
+++ cookiecutter-react-functional-component/hooks/post_gen_project.py
@@ -8,14 +8,14 @@ context = {{ cookiecutter }}
 def setup_stylesheets(stylesheet: str, component_name: str):
     # only keep the appropriate stylesheet file
     if stylesheet == 'css':
-        os.remove(f"{component_name}.scss")
+        os.remove(f"{component_name}.styles.scss")
         os.remove(f"{component_name}.styles.tsx")
     elif stylesheet == "scss":
-        os.remove(f"{component_name}.css")
+        os.remove(f"{component_name}.styles.css")
         os.remove(f"{component_name}.styles.tsx")
     elif stylesheet == "styled-components":
-        os.remove(f"{component_name}.css")
-        os.remove(f"{component_name}.scss")
+        os.remove(f"{component_name}.styles.css")
+        os.remove(f"{component_name}.styles.scss")
 
 def setup_testing(testing: str):
     # only keep the test files if requested
